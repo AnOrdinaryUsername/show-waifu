92c75d88257cf04073f5478559b773801bbb8e7f
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-06-23 23:42:52 -0700

    fix: Change module name to app

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-mod lib;
+mod api;
+mod app;
 
 fn main() {
-    if let Err(error) = lib::run() {
+    if let Err(error) = app::run() {
         eprintln!("{}", error);
         std::process::exit(1);
     }
