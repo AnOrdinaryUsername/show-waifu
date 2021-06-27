e005202718544a115aeba40fba5ad0a0a0839518
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-06-26 17:50:37 -0700

    fix: Replace commands with viuer library
    - Add support for stdin
    - Update and fix command descriptions

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,18 @@
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use viuer::{print_from_file, print};
 use clap::Clap;
-
+use atty::Stream;
 
 #[derive(Clap, Debug)]
 #[clap(name = "show-waifu", about = "View random anime fanart in your terminal!")]
 struct Cli {
     /// Resize the image to a provided height
     #[clap(short, long)]
-    height: Option<u16>,
+    height: Option<u32>,
 
     /// Resize the image to a provided width
     #[clap(short, long)]
-    width: Option<u16>,
+    width: Option<u32>,
 
     #[clap(subcommand)]
     command: Option<Subcommand>,
@@ -43,18 +43,18 @@ pub struct Random {
 
     /// Search for an image based on Safebooru tags.
     /// Pass as a string separated by spaces or commas.         
-    /// Look at Safebooru;s cheatsheet for a full list of search options
+    /// Look at Safebooru's cheatsheet for a full list of search options
     #[clap(short, long)]
     pub tags: Option<String>,
 }
 
-/// Pass a link for viewing
+/// View an image from a url
 #[derive(Clap, Debug)]
 struct Url {
     image_url: String,
 }
 
-/// Pass a local file for viewing
+/// View an image from your file system
 #[derive(Clap, Debug)]
  struct File {
     #[clap(parse(from_os_str), value_hint = clap::ValueHint::FilePath)]
@@ -67,52 +67,77 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     let Cli { width, height, .. } = args;
 
+    let config = viuer::Config {
+        width,
+        height,
+        absolute_offset: false,
+        ..Default::default()
+    };
+
+    // Read from stdin
+    if atty::isnt(Stream::Stdin) {
+        result = show_image_from_stdin(config);
+        return result;
+    }
+
     if let Some(command) = args.command {
         match command {
             Subcommand::Random(options) => {
-                result = show_random_image(options);
+                result = show_random_image(options, config);
             },
             Subcommand::File(file) => { 
-                result = show_image_with_path(file.file_path);
+                result = show_image_with_path(file.file_path, config);
             },
             Subcommand::Url(url) => {
-                result = show_image_with_url(url.image_url);
+                result = show_image_with_url(url.image_url, config);
             },
         };
     } else {
-        std::process::exit(1)
+        let default_options = Random {
+            details: false,
+            suggestive: false,
+            tags: None,
+        };
+
+        result = show_random_image(default_options, config);
     }
 
     result
 }
 
-fn show_random_image(args: Random) -> Result<(), Box<dyn std::error::Error>> {
+fn show_random_image(args: Random, config: viuer::Config) -> Result<(), Box<dyn std::error::Error>> {
     let image_url = crate::api::grab_random_image(args);
     let image_url = String::from(image_url);
 
-
-    show_image_with_url(image_url)
+    show_image_with_url(image_url, config)
 }
 
-fn show_image_with_url(image_url: String) -> Result<(), Box<dyn std::error::Error>> {
-    let curl = Command::new("curl")
-            .arg("-s")
-            .arg(image_url)
-            .stdout(Stdio::piped())
-            .spawn()?;
+fn show_image_with_url(image_url: String, config: viuer::Config) -> Result<(), Box<dyn std::error::Error>> {
+    let image_bytes = reqwest::blocking::get(&image_url)?.bytes()?;
+    let image = image::load_from_memory(&image_bytes)?;
 
-    Command::new("viu")
-        .arg("-")
-        .stdin(curl.stdout.unwrap())
-        .status()?;
-    
+    print(&image, &config)?;
+
     Ok(())
 }
 
-fn show_image_with_path(image_path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    Command::new("viu")
-        .arg(image_path)
-        .status()?;
+fn show_image_with_path(image_path: PathBuf, config: viuer::Config) -> Result<(), Box<dyn std::error::Error>> {
+    print_from_file(image_path, &config)?;
+
+    Ok(())
+}
+
+fn show_image_from_stdin(config: viuer::Config) -> Result<(), Box<dyn std::error::Error>> {
+    use std::io::{stdin, Read};
+
+    let stdin = stdin();
+    let mut handle = stdin.lock();
+
+    let mut buf: Vec<u8> = Vec::new();
+    let _ = handle.read_to_end(&mut buf)?;
+
+    let image = image::load_from_memory(&buf)?;
+    print(&image, &config)?;
 
     Ok(())
 }
